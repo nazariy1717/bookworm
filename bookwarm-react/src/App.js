c9ce7372d8442da1848506c36550306f2457b66d
nazariy17 <nazariy2410@gmail.com>
2017-10-16 17:31:20 +0300

    react router setup

--- bookwarm-react/src/App.js
+++ bookwarm-react/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 
-const App = () => <div>Hello world</div>;
+const App = () => <div>
+    <Route path="/" exact component={HomePage}></Route>
+    <Route path="/login" exact component={LoginPage}></Route>
+</div>;
 
 export default App;
