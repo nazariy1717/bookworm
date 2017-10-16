40c23a9480539268efc104616f8a12c6189bdca3
nazariy17 <nazariy2410@gmail.com>
2017-10-16 17:42:25 +0300

    semantic ui setup

--- bookwarm-react/src/App.js
+++ bookwarm-react/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 
 
-const App = () => <div>
-    <Route path="/" exact component={HomePage}></Route>
-    <Route path="/login" exact component={LoginPage}></Route>
-</div>;
+const App = () => (
+    <div className="ui container">
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/login" exact component={LoginPage}></Route>
+    </div>
+);
 
 export default App;
